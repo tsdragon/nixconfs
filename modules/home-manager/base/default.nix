d7c2f47f98f3411eb4ab1253f07d3bb6771333fe
Tal <tal@steakdrake.co>
2025-08-27 14:36:48 -0600

    manage default apps

--- modules/home-manager/base/default.nix
+++ modules/home-manager/base/default.nix
@@ -14,4 +14,28 @@
       mime.enable = true;
       systemDirs.data = [ "${config.home.homeDirectory}/.nix-profile/share/applications" ];
   };
+
+  # Manage mimeapps.list
+  home.file.".config/mimeapps.list".text = ''
+    [Added Associations]
+    application/x-shellscript=code.desktop;
+    text/plain=code.desktop;
+    text/x-nfo=code.desktop;
+    x-scheme-handler/tg=org.telegram.desktop.desktop;
+    x-scheme-handler/tonsite=org.telegram.desktop.desktop;
+
+    [Default Applications]
+    application/x-shellscript=code.desktop;
+    text/plain=code.desktop;
+    text/x-nfo=code.desktop;
+    x-scheme-handler/obsidian=obsidian.desktop;
+    x-scheme-handler/tg=org.telegram.desktop.desktop;
+    x-scheme-handler/tonsite=org.telegram.desktop.desktop;
+    video/mp4=vlc.desktop;
+    video/x-m4v=vlc.desktop;
+    video/x-msvideo=vlc.desktop;
+    video/x-matroska=vlc.desktop;
+    video/webm=vlc.desktop;
+    video/ogg=vlc.desktop;
+  '';
 }
