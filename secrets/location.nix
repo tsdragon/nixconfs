204339b497ac0159b7e99dad9d6a9d48cde4172b
Tal <tal@steakdrake.co>
2025-09-26 10:59:59 -0600

    update placeholder data

--- secrets/location.nix
+++ secrets/location.nix
@@ -1,3 +1,5 @@
+# BEFORE EDITING: run git update-index --skip-worktree /path/to/this/file
+# DO NOT COMMIT CHANGES TO THIS FILE, This is location data that could dox you if uploaded somewhere public.
 {
   myLocation = ''data:application/json,{"location": {"lat": 11.1111, "lng": -111.1111}, "accuracy": 1.0}'';
 }
