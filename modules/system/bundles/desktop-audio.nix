95cb116e0945c9cd27150fc6e442056f044d51bf
Tal <tal@steakdrake.co>
2025-10-09 09:31:00 -0600

    remove audio rate caps

--- modules/system/bundles/desktop-audio.nix
+++ modules/system/bundles/desktop-audio.nix
@@ -5,25 +5,12 @@
     "context.properties" = {
       "default.clock.rate" = 96000;
       "default.clock.allowed-rates" = [ 32000 44100 48000 88200 96000 ];
-      "default.clock.quantum" = 64;
+      "default.clock.quantum" = 128;
       "default.clock.min-quantum" = 64;
       "default.clock.max-quantum" = 256;
     };
 
-    context.modules = [
-      {
-        name = "libpipewire-module-protocol-pulse";
-        args = {
-          pulse.min.req = "64/48000";
-          pulse.default.req = "128/48000";
-          pulse.max.req = "256/48000";
-          pulse.min.quantum = "64/48000";
-          pulse.max.quantum = "256/48000";
-        };
-      }
-    ];
     stream.properties = {
-      node.latency = "64/48000";
       resample.quality = 10;
     };
   };
