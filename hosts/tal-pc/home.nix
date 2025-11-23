6d59c96e3ee6d4746ce7c785a99905bdd8cf5539
Tal <tal@steakdrake.co>
2025-11-22 20:15:07 -0700

    add ripping bundle

--- hosts/tal-pc/home.nix
+++ hosts/tal-pc/home.nix
@@ -6,6 +6,7 @@
     ../../modules/home-manager/base/identity.nix
     ../../modules/home-manager/bundles/3d.nix
     ../../modules/home-manager/bundles/messaging.nix
+    ../../modules/home-manager/bundles/ripping.nix
     ../../modules/home-manager/bundles/video_editing.nix
     ../../modules/home-manager/themes/kvantum_themes.nix
     ../../modules/home-manager/apps/zsh.nix
