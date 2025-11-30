af923f35796297afa13bfac058768fa5b76f14ef
Tal <tal@steakdrake.co>
2025-11-30 12:18:29 -0700

    add note about lychee fix

--- modules/home-manager/bundles/3d.nix
+++ modules/home-manager/bundles/3d.nix
@@ -1,5 +1,6 @@
 { config, pkgs, lib, ... }:
 
+# Fixes issue with LycheeSlicer not being found by desktop entry
 let
   lycheeWrapper = pkgs.writeShellScriptBin "lychee" ''
     exec LycheeSlicer "$@"
