047109ec67742191e48eb701dd70ae3c88922ea2
Tal <tal@steakdrake.co>
2026-01-13 15:41:16 -0700

    add streaming

---
+++ modules/home-manager/bundles/streaming.nix
@@ -0,0 +1,27 @@
+{ pkgs, ... }:
+
+{
+  # OBS Studio with plugins
+  programs.obs-studio = {
+    enable = true;
+    plugins = with pkgs.obs-studio-plugins; [
+      wlrobs
+      obs-backgroundremoval
+      obs-pipewire-audio-capture
+      obs-composite-blur
+      obs-shaderfilter
+      obs-scale-to-sound
+      obs-move-transition
+      obs-gradient-source
+      obs-replay-source
+      obs-source-clone
+      obs-3d-effect
+      obs-livesplit-one
+      waveform
+      obs-gstreamer
+      obs-vaapi
+      obs-vkcapture
+    ];
+  };
+
+}
