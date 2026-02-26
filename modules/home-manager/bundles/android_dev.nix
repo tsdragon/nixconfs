e1ec01b638b3a7427caa0e70d4c724424430db0a
Tal <tal@steakdrake.co>
2026-02-26 10:01:47 -0700

    add android dev environment

--- modules/home-manager/bundles/android_dev.nix
+++ modules/home-manager/bundles/android_dev.nix
@@ -2,22 +2,27 @@
 
 let
   androidPkgs = pkgs.androidenv.androidPkgs;
+  # Use a writable SDK path so Gradle can install missing components.
+  useNixSdk = false;
   hasSdk = androidPkgs ? androidsdk;
   sdkRoot =
-    if hasSdk
+    if useNixSdk && hasSdk
     then "${androidPkgs.androidsdk}/libexec/android-sdk"
     else "${config.home.homeDirectory}/Android/Sdk";
 
   androidSdkPackages =
-    lib.optionals hasSdk [ androidPkgs.androidsdk ]
-    ++ lib.optionals (!hasSdk) (lib.optionals (androidPkgs ? "cmdline-tools-latest") [
+    lib.optionals (useNixSdk && hasSdk) [ androidPkgs.androidsdk ]
+    ++ lib.optionals (!useNixSdk) (lib.optionals (androidPkgs ? "cmdline-tools-latest") [
       androidPkgs."cmdline-tools-latest"
     ])
-    ++ lib.optionals (!hasSdk) (lib.optionals (androidPkgs ? "platform-tools") [
+    ++ lib.optionals (!useNixSdk) (lib.optionals (androidPkgs ? "platform-tools") [
       androidPkgs."platform-tools"
     ]);
 in
 {
+
+  nixpkgs.config.android_sdk.accept_license = true;
+
   home.packages = with pkgs; [
     jdk17
     gradle
@@ -27,8 +32,8 @@ in
   ] ++ androidSdkPackages;
 
   home.sessionVariables = {
-    ANDROID_SDK_ROOT = sdkRoot;
-    ANDROID_HOME = sdkRoot;
+    ANDROID_SDK_ROOT = lib.mkDefault sdkRoot;
+    ANDROID_HOME = lib.mkDefault sdkRoot;
     JAVA_HOME = "${pkgs.jdk17}";
   };
 
