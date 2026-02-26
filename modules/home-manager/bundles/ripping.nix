ef442cafd8dd7f1ce6bdacb90e69076067b4a264
Tal <tal@steakdrake.co>
2026-02-26 10:02:03 -0700

    fix eac dependancy

--- modules/home-manager/bundles/ripping.nix
+++ modules/home-manager/bundles/ripping.nix
@@ -1,8 +1,13 @@
-{ config, pkgs, lib, ... }:
+{ config, pkgs, lib, inputs, ... }:
 
-{
+let
+  unstablePkgs = import inputs.nixpkgs-unstable {
+    system = pkgs.system;
+    config.allowUnfree = true;
+  };
+in {
   home.packages = with pkgs; [
-    exactaudiocopy
+    unstablePkgs.exactaudiocopy
     flac
     lame
     mktorrent
