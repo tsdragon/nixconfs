44b09c06ea44a9137bc164629e9efa84abc8456f
Tal <tal@steakdrake.co>
2026-03-09 23:30:33 -0600

    fix flac2all build

--- modules/home-manager/bundles/ripping.nix
+++ modules/home-manager/bundles/ripping.nix
@@ -1,16 +1,28 @@
 { config, pkgs, lib, inputs, ... }:
 
 let
-  unstablePkgs = import inputs.nixpkgs-unstable {
-    system = pkgs.system;
-    config.allowUnfree = true;
+  opusToolsCompat = pkgs.opus-tools.overrideAttrs (old: {
+    # libopus 1.6+ exports opus_check_* while opus-tools 0.2 expects __opus_check_*.
+    NIX_CFLAGS_COMPILE =
+      (old.NIX_CFLAGS_COMPILE or "")
+      + " -D__opus_check_int=opus_check_int -D__opus_check_int_ptr=opus_check_int_ptr";
+  });
+
+  flac2allCompat = pkgs.flac2all.override {
+    "opus-tools" = opusToolsCompat;
   };
-in {
+in
+
+{
   home.packages = with pkgs; [
-    unstablePkgs.exactaudiocopy
+    # EAC requires a full WoW Wine build; plain `wine` can miss functions on startup.
+    (exactaudiocopy.override {
+      wine = wineWow64Packages.stable;
+      use64 = true;
+    })
     flac
     lame
     mktorrent
-    flac2all
+    flac2allCompat
   ];
 }
