b5cfb1293c6fe29d1bf069aaa2137b3f6b261a6e
Tal <tal@steakdrake.co>
2026-03-18 10:22:24 -0600

    remove cludges from ripping apps

--- modules/home-manager/bundles/ripping.nix
+++ modules/home-manager/bundles/ripping.nix
@@ -1,28 +1,10 @@
-{ config, pkgs, lib, inputs, ... }:
-
-let
-  opusToolsCompat = pkgs.opus-tools.overrideAttrs (old: {
-    # libopus 1.6+ exports opus_check_* while opus-tools 0.2 expects __opus_check_*.
-    NIX_CFLAGS_COMPILE =
-      (old.NIX_CFLAGS_COMPILE or "")
-      + " -D__opus_check_int=opus_check_int -D__opus_check_int_ptr=opus_check_int_ptr";
-  });
-
-  flac2allCompat = pkgs.flac2all.override {
-    "opus-tools" = opusToolsCompat;
-  };
-in
-
+{ pkgs, pkgsUnstable, ... }:
 {
   home.packages = with pkgs; [
-    # EAC requires a full WoW Wine build; plain `wine` can miss functions on startup.
-    (exactaudiocopy.override {
-      wine = wineWow64Packages.stable;
-      use64 = true;
-    })
+    pkgsUnstable.exactaudiocopy
     flac
     lame
     mktorrent
-    flac2allCompat
+    flac2all
   ];
 }
