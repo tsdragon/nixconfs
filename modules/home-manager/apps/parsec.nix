b17a5e1d5822698284df63b719031800d1fecc63
Tal <tal@steakdrake.co>
2026-05-15 11:14:08 -0600

    fix parsec desktop entry

--- modules/home-manager/apps/parsec.nix
+++ modules/home-manager/apps/parsec.nix
@@ -18,22 +18,20 @@
         install -Dm0644 ${parsecNativeDesktopFile} $out/share/applications/parsecd.desktop
       '';
   });
-
-  parsecFlatpakDesktop = pkgs.makeDesktopItem {
-    name = "com.parsecgaming.parsec";
-    desktopName = "Parsec (Flatpak)";
-    genericName = "Parsec";
-    exec = "flatpak run --branch=stable --arch=x86_64 --command=/app/bin/parsec --file-forwarding com.parsecgaming.parsec @@u %u @@";
-    icon = "com.parsecgaming.parsec";
-    categories = ["Game"];
-    startupWMClass = "parsecd";
-    extraConfig = {
-      X-Flatpak = "com.parsecgaming.parsec";
-    };
-  };
 in {
   home.packages = [
     parsecNative
-    parsecFlatpakDesktop
   ];
+
+  home.file.".local/share/applications/com.parsecgaming.parsec.desktop".text = ''
+    [Desktop Entry]
+    Type=Application
+    Name=Parsec (Flatpak)
+    GenericName=Parsec
+    Icon=com.parsecgaming.parsec
+    Exec=flatpak run --branch=stable --arch=x86_64 --command=/app/bin/parsec --file-forwarding com.parsecgaming.parsec @@u %u @@
+    Categories=Game;
+    StartupWMClass=parsecd
+    X-Flatpak=com.parsecgaming.parsec
+  '';
 }
